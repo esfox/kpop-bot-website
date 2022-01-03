1ddd9c93fbd91c67dda78c3a97eb44bda12e311e
esfox <osairis100@gmail.com>
2022-01-03 13:15:05 +0800

    Update color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,6 @@ module.exports = {
           DEFAULT: 'var(--color-bg)',
           dark: 'var(--color-bg-dark)',
           darker: 'var(--color-bg-darker)',
-          'dark-hover': 'var(--color-bg-dark-hover)',
           light: 'var(--color-bg-light)',
           lighter: 'var(--color-bg-lighter)',
           lightest: 'var(--color-bg-lightest)',
@@ -22,7 +21,7 @@ module.exports = {
         white: 'var(--color-white)',
       },
       ringColor: {
-        DEFAULT: '#f6938a',
+        DEFAULT: '#62a5dd',
         primary: 'var(--color-primary)'
       },
     },
